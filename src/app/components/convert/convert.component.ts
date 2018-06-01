b7184a78c31116aa42d6f3650bae5fc54bf7d01c
Amit Pandey <pandey.amitx@gmail.com>
2018-06-01 15:02:00 +0530

    Changes for different bug fixes

--- src/app/components/convert/convert.component.ts
+++ src/app/components/convert/convert.component.ts
@@ -1,32 +1,31 @@
 import { Component, OnInit, ViewChild, ElementRef, HostListener} from '@angular/core';
 import { NG_VALIDATORS,Validator, Validators,AbstractControl,ValidatorFn } from '@angular/forms';
 import {DataService} from '../../service/data.service'
+import { EventEmitter } from 'events';
 
 @Component({
   selector: 'app-convert',
   templateUrl: './convert.component.html',
-  styleUrls: ['../../../../node_modules/@salesforce-ux/design-system/assets/styles/salesforce-lightning-design-system.min.css','./convert.component.css']
+  styleUrls: ['../../../../node_modules/@salesforce-ux/design-system/assets/styles/salesforce-lightning-design-system.min.css','./convert.component.scss']
 })
 export class ConvertComponent implements OnInit {
   currencies: any;
   currencyToBeChanged: number;
-  currType:number;
-  
-  private regex: RegExp = new RegExp(/^[0-9]*\.?[0-9]*$/g);
+  currType: number;
+  isDisclaimer: boolean = false;
+  discMsg: string;
+  regex = new RegExp("^[0-9]*\\.?[0-9]*$", "g");
   private specialKeys: Array<string> = [ 'Backspace', 'Tab', 'End', 'Home' ];
   
   @ViewChild('changeRate') changeRate: ElementRef;
   constructor(private dataservice: DataService) {
-    // console.log("in convert component constructor");
-    // console.log(dataservice.getConversionRate());
    }
    keys() : Array<string> {
     return Object.keys(this.currencies.rates);
   }
-   iamclicked(inputValue){
+  setChanges(inputValue){
      this.currencyToBeChanged=inputValue;
      this.currType = (this.changeRate.nativeElement.value * this.currencyToBeChanged);
-    //  console.log(this.changeRate.nativeElement.value);
    }
    currencyTypeChanged(event){
      if(this.currencyToBeChanged){
@@ -34,17 +33,18 @@ export class ConvertComponent implements OnInit {
      }
    }
    getDetails(event){
-     let selector=this.changeRate.nativeElement;
+    this.isDisclaimer=!this.isDisclaimer; 
+    let selector=this.changeRate.nativeElement;
     let text=selector.options[selector.selectedIndex].text;
-    //  console.log(this.changeRate.nativeElement);
-    alert("Current Change Rate for "+ text +" Is: "+this.changeRate.nativeElement.value);
+    this.discMsg="Change Rate : "+this.changeRate.nativeElement.value;
+    // alert("Current Change Rate for "+ text +" Is: "+this.changeRate.nativeElement.value);
    }
     @HostListener('keydown', [ '$event' ])
-    onKeyDown(event: KeyboardEvent) {
+    onKeyDown(event) {
         if (this.specialKeys.indexOf(event.key) !== -1) {
             return;
         }
-        let current: string = this.changeRate.nativeElement.value;
+        let current: string = event.target.value;
         let next: string = current.concat(event.key);
         if (next && !String(next).match(this.regex)) {
             event.preventDefault();
@@ -52,7 +52,6 @@ export class ConvertComponent implements OnInit {
     }
   ngOnInit() {
     this.dataservice.getConversionRate().subscribe((currencyjosn)=>{
-      // console.log(currencyjosn);
       this.currencies=currencyjosn;
     });
   }
--- src/app/service/data.service.ts
+++ src/app/service/data.service.ts
@@ -7,6 +7,6 @@ export class DataService {
     console.log("data service connected");
   }
   getConversionRate(){
-    return this.http.get("http://data.fixer.io/api/latest?access_key=5de68d7904f8f501bb47d85c9dec718e&format=1&&symbols=CAD,USD,EUR,AUD").map(res=> res.json());
+    return this.http.get("http://data.fixer.io/api/latest?access_key=5de68d7904f8f501bb47d85c9dec718e&format=1&&symbols=CAD,USD,EUR").map(res=> res.json());
   }
 }
